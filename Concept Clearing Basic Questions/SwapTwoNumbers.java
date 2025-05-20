ff2d4857476da5ac2d7ab08e7db146fed63338a1
Sujay <powar.sujay@koptotech.com>
2025-05-20 14:57:37 +0530

    basic concept changes

---
+++ Concept Clearing Basic Questions/SwapTwoNumbers.java
@@ -0,0 +1,22 @@
+public class SwapTwoNumbers {
+    public static void main(String[] args) {
+        int a=10;
+        int b=30;
+        int temp = 0;
+//         Method 1
+//         temp = a;
+//         a = b;
+//         b = temp;
+
+
+//        Method 2
+//         a = a + b;
+//         b = a - b;
+//         a = a - b;
+
+
+        System.out.println("The value of A is :- " + a);
+        System.out.println("The value of B is :- " + b);
+
+    }
+}
